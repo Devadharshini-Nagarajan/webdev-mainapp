2997e1c6bd4a6c0839c5cec15b06a39f14fccd8a
Devadharshini-Nagarajan <nagarajan.de@northeastern.edu>
2023-04-23 08:17:17 -0400

    feat: leaflet map setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Router from "./router";
 
 import Context from "./components/Context";
+import "leaflet/dist/leaflet.css";
 // import "antd/dist/antd.css";
 import "./App.css";
 
